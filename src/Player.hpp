fd9e870b0ba86a452c9250a139d39ad2bd0a22c4
danquist <dan.quist@gmail.com>
2014-04-12 20:47:44 -0400

    Update license to MIT and fix header guards

--- src/Player.hpp
+++ src/Player.hpp
@@ -1,18 +1,32 @@
 /*
- * BaseEvent.hpp
+ * Copyright (c) 2014, Dan Quist
  *
- *  Created on: Oct 19, 2013
- *      Author: Dan
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-
-#include <string>
-
-#ifndef _PLAYER_HPP_
-#define _PLAYER_HPP_
+#ifndef _SRC_PLAYER_HPP_
+#define _SRC_PLAYER_HPP_
 
 #include "Object.hpp"
 #include "PlayerMoveEvent.hpp"
+
+#include <string>
 
 class Player : public Object
 {
@@ -22,9 +36,7 @@ public:
 		posX(0),
 		posY(0),
 		posZ(0)
-	{
-
-	}
+	{ }
 
 	virtual ~Player() {
 
@@ -78,4 +90,4 @@ private:
 };
 
 
-#endif /* _PLAYER_HPP_ */
+#endif /* _SRC_PLAYER_HPP_ */
--- src/event/HandlerRegistration.hpp
+++ src/event/HandlerRegistration.hpp
@@ -1,12 +1,27 @@
 /*
- * Object.hpp
+ * Copyright (c) 2014, Dan Quist
  *
- *  Created on: Oct 19, 2013
- *      Author: Dan
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-#ifndef HANDLER_REGISTRATION_HPP_
-#define HANDLER_REGISTRATION_HPP_
+#ifndef _SRC_EVENT_HANDLER_REGISTRATION_HPP_
+#define _SRC_EVENT_HANDLER_REGISTRATION_HPP_
 
 #include "Object.hpp"
 
@@ -17,4 +32,4 @@ public:
 	virtual void removeHandler() = 0;
 };
 
-#endif /* HANDLER_REGISTRATION_HPP_ */
+#endif /* _SRC_EVENT_HANDLER_REGISTRATION_HPP_ */
